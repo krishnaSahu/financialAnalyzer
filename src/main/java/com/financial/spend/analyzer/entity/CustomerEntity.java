b9676b29ec7abfbc29769e9bc3c89b519495bf90
Krishnakumar Sahu <kumarkrishnasahu@gmail.com>
2021-06-26 15:43:27 +0530

    Customer entity created

--- src/main/java/com/financial/spend/analyzer/entity/CustomerEntity.java
+++ src/main/java/com/financial/spend/analyzer/entity/CustomerEntity.java
@@ -1,5 +1,82 @@
 package com.financial.spend.analyzer.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "customer")
 public class CustomerEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "c_cust_custid", columnDefinition = "serial")
+	private Long customerId;
+
+	@Column(name = "a_cust_custname", nullable = false)
+	private String name;
+
+	@Column(name = "a_cust_password", nullable = false)
+	private String password;
+
+	@Column(name = "a_cust_mobnum", nullable = false)
+	private String mobileNumber;
+
+	@Column(name = "a_cust_emailid", nullable = true)
+	private String emailId;
+
+	@Column(name = "b_cust_isactive", nullable = false)
+	private Boolean active;
+
+	public Long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getMobileNumber() {
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber) {
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
 
 }
