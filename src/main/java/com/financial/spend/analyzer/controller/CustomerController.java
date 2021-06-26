14cb2c20addd0100ec571c4ccb69e7ca96cbda71
Krishnakumar Sahu <kumarkrishnasahu@gmail.com>
2021-06-26 17:42:57 +0530

    Utility Classes modified

--- src/main/java/com/financial/spend/analyzer/controller/CustomerController.java
+++ src/main/java/com/financial/spend/analyzer/controller/CustomerController.java
@@ -1,13 +1,16 @@
 package com.financial.spend.analyzer.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.financial.spend.analyzer.dto.CustomerDto;
 
 @RestController
 public class CustomerController {
 
 	@GetMapping("/customer")
-	public String getCustomerId() {
+	public String getCustomerId(@RequestBody CustomerDto customerDto) {
 
 		return "1";
 
