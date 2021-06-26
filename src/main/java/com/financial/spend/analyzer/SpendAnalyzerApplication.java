fa347c9b4991460d42b596e7099da93ee81d5cf0
Krishnakumar Sahu <kumarkrishnasahu@gmail.com>
2021-06-26 14:46:21 +0530

    Financial git repo initialize

---
+++ src/main/java/com/financial/spend/analyzer/SpendAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.financial.spend.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financial/spend/analyzer/SpendAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financial.spend.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
